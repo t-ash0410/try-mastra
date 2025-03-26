cfd37d44e6f21e720d6b78a848f5a3cb757592da
t-ash0410 <t_ash0410@yahoo.co.jp>
2025-03-27 07:41:54 +0900

    setup flake

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        gcloud = pkgs.google-cloud-sdk.withExtraComponents [ 
+          pkgs.google-cloud-sdk.components.gke-gcloud-auth-plugin 
+        ];
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            bun
+          ];
+        };
+      }
+    );
+}
